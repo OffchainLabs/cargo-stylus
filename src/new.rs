9a3e8969ec0f1334746f5c823cd0d408fa028140
Raul Jordan <raul@prysmaticlabs.com>
2023-08-25 16:38:49 -0700

    readme

--- src/new.rs
+++ src/new.rs
@@ -27,6 +27,19 @@ pub fn new_stylus_project(name: &str, minimal: bool) -> Result<(), String> {
             .output()
             .map_err(|e| format!("failed to execute cargo new: {e}"))?;
 
+        let cargo_config_dir_path = cwd.join(name).join(".cargo");
+        std::fs::create_dir_all(&cargo_config_dir_path)
+            .map_err(|e| format!("could not create .cargo folder in new project: {e}"))?;
+        let cargo_config_path = cargo_config_dir_path.join("config");
+        let mut f = std::fs::OpenOptions::new()
+            .write(true)
+            .open(cargo_config_path)
+            .map_err(|e| format!("could not open config file: {e}"))?;
+        f.write_all(cargo_config().as_bytes())
+            .map_err(|e| format!("could not write to file: {e}"))?;
+        f.flush()
+            .map_err(|e| format!("could not write to file: {e}"))?;
+
         let main_path = cwd.join(name).join("src").join("main.rs");
 
         // Overwrite the default main.rs file with the Stylus entrypoint.
@@ -77,6 +90,12 @@ fn user_main(input: Vec<u8>) -> Result<Vec<u8>, Vec<u8>> {
     Ok(input)
 }
 "#
+}
+
+fn cargo_config() -> &'static str {
+    r#"[build]
+target = "wasm32-unknown-unknown"
+    "
 }
 
 fn minimal_cargo_toml(name: &str) -> String {
