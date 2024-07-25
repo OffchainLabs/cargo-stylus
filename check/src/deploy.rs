4dbb3006026e508d13223058d4cf45644c780227
Raul Jordan <raul@prysmaticlabs.com>
2024-07-25 17:11:27 -0500

    edits

--- check/src/deploy.rs
+++ check/src/deploy.rs
@@ -50,23 +50,6 @@ pub async fn deploy(cfg: DeployConfig) -> Result<()> {
         };
     }
 
-    // Get the active toolchain using rustup
-    let active_toolchain_output = Command::new("rustup")
-        .arg("show")
-        .arg("active-toolchain")
-        .output()
-        .expect("Failed to execute rustup command");
-    let active_toolchain = String::from_utf8_lossy(&active_toolchain_output.stdout);
-    greyln!("Active Rust toolchain: {}", active_toolchain.trim());
-
-    // Get the rustc version
-    let rustc_version_output = Command::new("rustc")
-        .arg("--version")
-        .output()
-        .expect("Failed to execute rustc command");
-    let rustc_version = String::from_utf8_lossy(&rustc_version_output.stdout);
-    greyln!("Using rustc version: {}", rustc_version.trim());
-
     let program = run!(check::check(&cfg.check_config), "cargo stylus check failed");
     let verbose = cfg.check_config.common_cfg.verbose;
 
@@ -159,6 +142,22 @@ impl DeployConfig {
             greyln!("deployed code at address: {address}");
         }
         let tx_hash = receipt.transaction_hash.debug_lavender();
+        // Get the active toolchain using rustup
+        let active_toolchain_output = Command::new("rustup")
+            .arg("show")
+            .arg("active-toolchain")
+            .output()
+            .expect("Failed to execute rustup command");
+        let active_toolchain = String::from_utf8_lossy(&active_toolchain_output.stdout);
+        greyln!("Active Rust toolchain: {}", active_toolchain.trim());
+
+        // Get the rustc version
+        let rustc_version_output = Command::new("rustc")
+            .arg("--version")
+            .output()
+            .expect("Failed to execute rustc command");
+        let rustc_version = String::from_utf8_lossy(&rustc_version_output.stdout);
+        greyln!("Using rustc version: {}", rustc_version.trim());
         greyln!("Deployment tx hash: {tx_hash}");
         Ok(contract)
     }
