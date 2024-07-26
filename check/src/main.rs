46bff53a0df3f3b79c81ba12e31b988eaa5860ad
Raul Jordan <raul@prysmaticlabs.com>
2024-07-26 11:11:02 -0500

    Update check/src/main.rs
    Co-authored-by: Joshua Colvin <jcolvin@offchainlabs.com>

--- check/src/main.rs
+++ check/src/main.rs
@@ -138,7 +138,7 @@ pub struct VerifyConfig {
     deployment_tx: String,
     #[arg(long)]
     /// If specified, will not run the command in a reproducible docker container. Useful for local
-    /// builds, but at the risk of not having a reproducible command for verification purposes.
+    /// builds, but at the risk of not having a reproducible contract for verification purposes.
     no_verify: bool,
 }
 
