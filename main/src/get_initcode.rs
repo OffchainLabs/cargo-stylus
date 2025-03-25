8f7da12391228687b3fc932f015f86cbb3b0e9ac
Yash <kumaryashcse@gmail.com>
2025-03-26 01:40:55 +0530

    license header

--- main/src/get_initcode.rs
+++ main/src/get_initcode.rs
@@ -1,3 +1,6 @@
+// Copyright 2025, Offchain Labs, Inc.
+// For licensing, see https://github.com/OffchainLabs/cargo-stylus/blob/main/licenses/COPYRIGHT.md
+
 use std::{fs::File, io::Write};
 use eyre::{Result, WrapErr};
 use crate::{deploy::contract_deployment_calldata, project, GetInitcodeConfig};
